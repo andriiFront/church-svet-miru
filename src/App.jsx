eacbf13b5328e5ef0f67bdcf92626e0a6fcf3c6f
Andrii <badekha@gmail.com>
2022-09-01 19:57:48 +0300

    added other blocks in first edition

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
-// import { Main } from './components/Main';
-// import { Footer } from './components/Footer';
+import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-
-     <h1 className="App-header"_>Svet Miru</h1>
-    <Header />
-    {/* <Main /> */}
-    {/* <Footer /> */}
+      <Header />
+      <Main />
+      <Footer />
   </div>
   );
 }
